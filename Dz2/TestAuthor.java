16237a3382da840e985a1bd77224d6a53dd41d4f
Stone-by <56612976+Stone-by@users.noreply.github.com>
2021-02-12 16:48:41 +0300

    Add files via upload

---
+++ Dz2/TestAuthor.java
@@ -0,0 +1,9 @@
+package ikbo08192;
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+        Author c1 = new Author("Mike", "fastscrul@gmail.com", 'm');
+        System.out.println(c1);
+    }
+}
---
+++ Dz2/TestBall.java
@@ -0,0 +1,9 @@
+package ikbo08192;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball c1 = new Ball(15,3);
+        c1.move(3,5);
+        System.out.println(c1);
+    }
+}
